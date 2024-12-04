5a3e710b75fed647a61b03195508142c8387e7b0
yassyass2 <yassinabde@outlook.com>
2024-12-04 23:32:34 +0100

    day 3 part 2 working, wrong output

--- aoc 2024/day3/day3.js
+++ aoc 2024/day3/day3.js
@@ -46,13 +46,24 @@ var day3;
     const input = loadFileLines("./day3.txt");
     function findAllMulPatterns(text) {
         const pattern = /mul\((\d{1,3}),(\d{1,3})\)/g;
+        const doPattern = /do\(\)/g;
+        const dontPattern = /don't\(\)/g;
         const results = [];
-        for (const match of text.matchAll(pattern)) {
-            const num1 = parseInt(match[1], 10);
-            //console.log(num1)
-            const num2 = parseInt(match[2], 10);
-            //console.log(num2)
-            results.push(num1 * num2);
+        let enabled = true;
+        const matches = text.matchAll(/mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/g);
+        for (const match of matches) {
+            const [fullMatch, num1, num2] = match;
+            if (dontPattern.test(fullMatch)) {
+                enabled = false;
+            }
+            else if (doPattern.test(fullMatch)) {
+                enabled = true;
+            }
+            else if (pattern.test(fullMatch) && enabled) {
+                const n1 = parseInt(num1, 10);
+                const n2 = parseInt(num2, 10);
+                results.push(n1 * n2);
+            }
         }
         return Fold((x, y) => x + y, 0)(results);
     }
--- aoc 2024/day3/day3.ts
+++ aoc 2024/day3/day3.ts
@@ -19,15 +19,24 @@ namespace day3{
     const input = loadFileLines("./day3.txt")
 
     function findAllMulPatterns(text: string): number {
-        const pattern = /mul\((\d{1,3}),(\d{1,3})\)/g;
+        const pattern: RegExp = /mul\((\d{1,3}),(\d{1,3})\)/g;
+        const doPattern: RegExp = /do\(\)/g
+        const dontPattern: RegExp = /don't\(\)/g
         const results: number[] = [];
     
-        for (const match of text.matchAll(pattern)) {
-            const num1 = parseInt(match[1], 10);
-            //console.log(num1)
-            const num2 = parseInt(match[2], 10);
-            //console.log(num2)
-            results.push(num1*num2);
+        let enabled = true
+        const matches = text.matchAll(/mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/g)
+        for (const match of matches) {
+            const [fullMatch, num1, num2] = match;
+            if (dontPattern.test(fullMatch)) {
+                enabled = false;
+            } else if (doPattern.test(fullMatch)) {
+                enabled = true;
+            } else if (pattern.test(fullMatch) && enabled){
+                const n1 = parseInt(num1, 10);
+                const n2 = parseInt(num2, 10);
+                results.push(n1*n2);
+            }
         }
     
         return Fold<number, number>((x: number, y: number) => x+y, 0)(results);
