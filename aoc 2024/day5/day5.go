a60766cea206cecca2ad1ae6eecb2acd9866c066
yassyass2 <yassinabde@outlook.com>
2024-12-10 23:28:05 +0100

    input processing for day 5

---
+++ aoc 2024/day5/day5.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+func readInput(filename string) ([][]string, [][]string) {
+	rules := [][]string{}
+	sequence := [][]string{}
+
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return rules, sequence
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if strings.Contains(line, "|") {
+			rules = append(rules, strings.Split(line, "|"))
+		} else if strings.Contains(line, ",") {
+			sequence = append(sequence, strings.Split(line, ","))
+		}
+	}
+
+	return rules, sequence
+}
