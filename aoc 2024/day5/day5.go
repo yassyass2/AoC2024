5d9e9360c7fa4c6f261bc0d3cb85100ead4c0812
yassyass2 <yassinabde@outlook.com>
2024-12-11 23:47:06 +0100

    day 5 1/2

--- aoc 2024/day5/day5.go
+++ aoc 2024/day5/day5.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -31,12 +33,47 @@ func readInput(filename string) ([][]string, [][]string) {
 	return rules, sequence
 }
 
-func countMiddles(sequence [][]string, rules [][]string) {
-	total := 0
-	for i, instance := range sequence {
-
-		for j, value := range instance {
-
+func AbidesByAllRules(update []string, rules [][]string) bool {
+	firstIndex := -1
+	secondIndex := -1
+	for i, value := range update {
+		if value == rules[0][0] && firstIndex == -1 {
+			firstIndex = i
+		}
+		if value == rules[0][1] && secondIndex == -1 {
+			secondIndex = i
+		}
+		if firstIndex != -1 && secondIndex != -1 {
+			break
 		}
 	}
+	if firstIndex == -1 || secondIndex == -1 || firstIndex < secondIndex {
+		if len(rules) > 1 {
+			return true && AbidesByAllRules(update, rules[1:])
+		}
+		return true
+	}
+	return false
+}
+
+func countMiddles(sequence [][]string, rules [][]string) int {
+	total := 0
+	for _, CurrentUpdate := range sequence {
+
+		if AbidesByAllRules(CurrentUpdate, rules) {
+			ToAdd, err := strconv.Atoi(CurrentUpdate[(len(CurrentUpdate)-1)/2])
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+			total += ToAdd
+		}
+	}
+	return total
+}
+
+func main() {
+	rules, sequence := readInput("day5.txt")
+
+	result := countMiddles(sequence, rules)
+	fmt.Printf("%d", result)
 }
