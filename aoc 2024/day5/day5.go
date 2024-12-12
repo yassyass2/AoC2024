c22520352903401a61a0d7d2e9ec5bce2dbce489
yassyass2 <yassinabde@outlook.com>
2024-12-12 23:43:37 +0100

    day 5 part 1 en 2 go

--- aoc 2024/day5/day5.go
+++ aoc 2024/day5/day5.go
@@ -56,8 +56,34 @@ func AbidesByAllRules(update []string, rules [][]string) bool {
 	return false
 }
 
-func countMiddles(sequence [][]string, rules [][]string) int {
+func Swapper(update []string, rules [][]string) []string {
+	firstIndex := -1
+	secondIndex := -1
+	for i, value := range update {
+		if value == rules[0][0] && firstIndex == -1 {
+			firstIndex = i
+		}
+		if value == rules[0][1] && secondIndex == -1 {
+			secondIndex = i
+		}
+		if firstIndex != -1 && secondIndex != -1 {
+			break
+		}
+	}
+	if firstIndex != -1 && secondIndex != -1 && firstIndex > secondIndex {
+		firstVal := update[firstIndex]
+		update[firstIndex] = update[secondIndex]
+		update[secondIndex] = firstVal
+	}
+	if len(rules) > 1 {
+		return Swapper(update, rules[1:])
+	}
+	return update
+}
+
+func countMiddles(sequence [][]string, rules [][]string) []int {
 	total := 0
+	swapped := 0
 	for _, CurrentUpdate := range sequence {
 
 		if AbidesByAllRules(CurrentUpdate, rules) {
@@ -66,14 +92,25 @@ func countMiddles(sequence [][]string, rules [][]string) int {
 				fmt.Println("Error:", err)
 			}
 			total += ToAdd
+		} else {
+			for !AbidesByAllRules(CurrentUpdate, rules) {
+				CurrentUpdate = Swapper(CurrentUpdate, rules)
+			}
+			ToSwap, err := strconv.Atoi(CurrentUpdate[(len(CurrentUpdate)-1)/2])
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+			swapped += ToSwap
 		}
 	}
-	return total
+	return []int{total, swapped}
 }
 
 func main() {
 	rules, sequence := readInput("day5.txt")
 
 	result := countMiddles(sequence, rules)
-	fmt.Printf("%d", result)
+	fmt.Printf("%d", result[0])
+	fmt.Printf(" ")
+	fmt.Printf("%d", result[1])
 }
