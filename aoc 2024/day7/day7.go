734018de682501bd8bdaa328bc3b558df82dbde7
yassyass2 <yassinabde@outlook.com>
2024-12-28 23:58:11 +0100

    string conversion

--- aoc 2024/day7/day7.go
+++ aoc 2024/day7/day7.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"strconv"
 )
 
 func readInput(filename string) [][]string {
@@ -26,8 +27,29 @@ func readInput(filename string) [][]string {
 	return calibrations
 }
 
+func CheckSum(aim int, products []int) bool{
+	return true
+}
+
 func calculateResult(calibs [][]string) int {
 	total := 0
+	for _, val := range calibs{
+		aim := val[0]
+		val[0] = aim[:len(aim)-1]
+
+		steps := make([]int, len(val))
+
+		for i, str := range val {
+			num, err := strconv.Atoi(str)
+			if err != nil {
+				fmt.Printf("Error converting %q to int: %v\n", str, err)
+				return 0
+			}
+			steps[i] = num
+		}
+
+		if CheckSum()()
+	}
 	return total
 }
 
