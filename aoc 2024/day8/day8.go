5f989c49541d7adbde08aeebb57e9aa994ad4656
yassyass2 <yassinabde@outlook.com>
2025-02-05 23:53:01 +0100

    somewhat acceptable way for antinode lookup, no duplicate check

--- aoc 2024/day8/day8.go
+++ aoc 2024/day8/day8.go
@@ -42,6 +42,33 @@ func getMappedNodes(Map [][]string) map[string][][]int {
 	return MappedNodes
 }
 
+func specificNode(cords [][]int) [][]int {
+	resultSet := [][]int{}
+	for index, cord := range cords {
+		for ind, coor := range cords {
+			if index == ind {
+				continue
+			}
+			underY := cord[0] - (cord[0] - coor[0])
+			underX := cord[1] - (cord[1] - coor[1])
+			upperY := cord[0] + (cord[0] - coor[0])
+			upperX := cord[1] + (cord[1] - coor[1])
+			if underY >= 0 && underY <= 49 && underX >= 0 && underX <= 49 {
+				resultSet = append(resultSet, []int{underY, underX})
+			}
+			if upperY >= 0 && upperY <= 49 && upperX >= 0 && upperX <= 49 {
+				resultSet = append(resultSet, []int{upperY, upperX})
+			}
+		}
+	}
+	return resultSet
+}
+
 func main() {
-	fmt.Print(getMappedNodes(readInput("day8.txt")))
+	original := readInput("day8.txt")
+	mapped := getMappedNodes(original)
+	fmt.Print(mapped)
+	for _, value := range mapped {
+		specificNode(value)
+	}
 }
